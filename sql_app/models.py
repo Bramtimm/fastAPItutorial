bb22b51de37b7650fa3956bdbb8d9edc46cd3aa0
bramtimm <bram.timm@gmail.com>
2021-06-13 20:50:11 +0200

    trying out get/update/post

--- sql_app/models.py
+++ sql_app/models.py
@@ -9,6 +9,6 @@ class UserInfo(Base):
     __tablename__ = "user_info"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    username = Column(String, unique=True)
     password = Column(String)
     fullname = Column(String, unique=True)
